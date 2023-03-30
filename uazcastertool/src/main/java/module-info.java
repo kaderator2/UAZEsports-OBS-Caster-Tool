033f1a30e64c9e6efb0dbcae7d67f792533b6427
Kade Hugh <77413927+kaderator2@users.noreply.github.com>
2023-03-29 18:17:54 -0700

    Delete uazcastertool directory

--- uazcastertool/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.uazesportsobscastertool {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.uazesportsobscastertool to javafx.fxml;
-    exports com.example.uazesportsobscastertool;
-}
